824ae79f1f0c0af84088a064e0e8266ade9a2a2e
kitesoft2058 <96kite@gmail.com>
2024-05-26 21:41:19 +0900

    .

--- src/Main.js
+++ src/Main.js
@@ -10,6 +10,8 @@ const Main= ()=>{
         controls.start('open')
     }
 
+    const aa=new Array(10).fill().map((v,i)=>i+1)
+
     return (
         <div>
             {/*(실습1) 카카오 지도를 보여주는 컴포넌트 */}
@@ -27,7 +29,28 @@ const Main= ()=>{
                 drag='y'
                 dragConstraints={{top:0}}
                 >
+                
                 <BottomSheetHandle></BottomSheetHandle>
+
+                <BottomSheetContent>
+                    <div className="text">
+                        <input placeholder="title"></input>
+                        <textarea placeholder="memo"></textarea>
+                    </div>
+
+                    <div className="photos">
+                        {
+                           aa.map(v=><div>{v}</div>) 
+                        }
+                    </div>
+
+                    <div className="buttons">
+                        <button onClick={()=>controls.start('close')}>작성완료</button>
+                    </div>
+
+                </BottomSheetContent>
+
+                
             </BottomSheet>
 
 
@@ -48,6 +71,9 @@ const BottomSheet = styled(motion.div)`
     box-shadow: 0 0 10px gray;
     border-top-left-radius: 16px;
     border-top-right-radius: 16px;
+    padding: 8px;
+    width: 96%;
+    margin: 0 auto;
 `
 
 // bottom sheet handle
@@ -57,4 +83,65 @@ const BottomSheetHandle = styled.div`
     border-radius: 4px;
     background-color: silver;
     margin: 16px auto;
+`
+
+// bottom sheet content
+const BottomSheetContent= styled.div`
+    width: 100%;
+    box-sizing: border-box;
+    padding: 16px;
+
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;
+
+
+    .text{
+        width: 45%;  
+        height: 200px;
+        //border: 1px dotted red;
+        display: flex;
+        flex-direction: column;
+
+        input{
+            width: 100%;
+            box-sizing: border-box;
+            padding: 0.5rem;
+            margin-bottom: 8px;
+        }
+
+        textarea{
+            width: 100%;
+            box-sizing: border-box;
+            padding: 0.5rem;
+            resize: none;
+            flex: 1;
+        }
+    }
+
+    .photos{
+        width: 45%;
+        //border: 1px dotted blue;
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+
+    }
+
+    .buttons{
+        width: 100%;
+        margin-top: 8px;
+        //border: 1px dotted green;
+
+        button{
+            width: 100%;
+            padding: 0.8rem;
+            background-color: lightblue;
+            border: 1px solid black;
+            border-radius: 4px;
+        }
+    }
+    
+
 `
